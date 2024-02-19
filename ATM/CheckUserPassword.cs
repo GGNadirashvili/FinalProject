3e07113a6d2f0447967fc721175a3fdecbdf200c
ggnadirashvilii <ggnadirashvilii@gmail.com>
2024-02-19 15:53:28 +0400

    Read Data From .txt & Check User CardNumber and PinCode

---
+++ ATM/CheckUserPassword.cs
@@ -0,0 +1,21 @@
+﻿namespace ATM
+{
+    public class CheckUserPassword
+    {
+        public static bool CheckPassword(List<User> users, int cardNumber, int enteredPinCode)
+        {
+            User user = users.FirstOrDefault(user => user.CardNumber == cardNumber)!;
+
+            if (user.PinCode == enteredPinCode)
+            {
+                Console.WriteLine("Password correct. Access granted.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password. Access denied.");
+                return false;
+            }
+        }
+    }
+}
